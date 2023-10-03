2726b2ccfa08be9c61c70fdffa32a1ffa438f87a
Kyle Nichols <kyle.r.nichols@gmail.com>
2023-10-03 13:37:44 +0900

    modified main.py file

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-# This is a sample Python script.
+user_prompt = "Enter a new todo: "
+title = input(user_prompt)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(len(title))
+print(type(title))
