277e7bb6e00d56112db1ec4e6ac6cc10cc95e500
Kyle Nichols <kyle.r.nichols@gmail.com>
2023-10-04 13:20:53 +0900

    todo list with terminal menu

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-user_prompt = "Enter a new todo: "
-title = input(user_prompt)
+todos = []
 
-print(len(title))
-print(type(title))
+while True:
+    user_action = input("Enter add, show, edit, or exit: ")
+
+    match user_action.strip():
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show' | 'display':
+            for item in todos:
+                print(item)
+        case 'edit':
+            print(todos)
+            number = int(input("Choose which todo to edit: "))
+            updated_todo = input("Enter the new todo: ")
+            todos[number -1] = updated_todo
+        case 'exit':
+            break
+
+
+print('Goodbye!')
