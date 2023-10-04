8fd2504d3f8bc2dbbe0348c028a28e8122cc4c49
Kyle Nichols <kyle.r.nichols@gmail.com>
2023-10-04 16:25:30 +0900

    added 'complete' to menu

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 todos = []
 
 while True:
-    user_action = input("Enter add, show, edit, or exit: ")
+    user_action = input("Enter add, show, edit, complete, or exit: ")
 
     match user_action.strip():
         case 'add':
             todo = input("Enter a todo: ")
             todos.append(todo)
         case 'show' | 'display':
-            for item in todos:
-                print(item)
+            for index, todo in enumerate(todos):
+                print(f"{index + 1} - {todo}")
         case 'edit':
-            print(todos)
+            for index, todo in enumerate(todos):
+                print(f"{index + 1} - {todo}")
             number = int(input("Choose which todo to edit: "))
             updated_todo = input("Enter the new todo: ")
-            todos[number -1] = updated_todo
+            todos[number - 1] = updated_todo
+        case 'complete':
+            number = int(input("Choose which todo to edit: "))
+            todos.pop(number - 1)
         case 'exit':
             break
 
