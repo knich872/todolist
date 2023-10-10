27b7d40d455519af0aed16e95d7aaa51a67976b6
Kyle Nichols <kyle.r.nichols@gmail.com>
2023-10-10 09:34:42 +0900

    removed hard coded file path from get/save functions

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-def get_todos():
-    with open('todos.txt', 'r') as file:
+def get_todos(filepath):
+    with open(filepath, 'r') as file:
         todos_local = file.readlines()
     return todos_local
 
 
-def save_todos(todos_to_save):
-    with open('todos.txt', 'w') as file:
+def save_todos(filepath, todos_to_save):
+    with open(filepath, 'w') as file:
         file.writelines(todos_to_save)
 
+file = "todos.txt"
 
 while True:
     user_action = input("Enter add, show, edit, complete, or exit: ").strip()
@@ -15,14 +16,14 @@ while True:
     if user_action.startswith('add'):
         todo = user_action[4:] + '\n'
 
-        todos = get_todos()
+        todos = get_todos(file)
 
         todos.append(todo)
 
-        save_todos(todos)
+        save_todos(file, todos)
 
     elif user_action.startswith('show'):
-        todos = get_todos()
+        todos = get_todos(file)
 
         for index, todo in enumerate(todos):
             todo = todo.strip('\n')
@@ -31,12 +32,12 @@ while True:
         try:
             number = int(user_action[5:])
 
-            todos = get_todos()
+            todos = get_todos(file)
 
             updated_todo = input("Enter the new todo: ")
             todos[number - 1] = updated_todo + '\n'
 
-            save_todos(todos)
+            save_todos(file, todos)
 
         except ValueError:
             print("Your command is not valid")
@@ -46,14 +47,14 @@ while True:
         try:
             number = int(user_action[9:])
 
-            todos = get_todos()
+            todos = get_todos(file)
 
             completed = todos[number - 1].strip('\n')
             print(f"{completed} was removed from the list")
 
             todos.pop(number - 1)
 
-            save_todos(todos)
+            save_todos(file, todos)
 
         except IndexError:
             print("There is no todo with that number")
