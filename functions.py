1ac776b8f6f2ed142b3af204821a4f08982ac72a
Kyle Nichols <kyle.r.nichols@gmail.com>
2023-10-11 17:10:35 +0900

    modified the GUI to accept new todos and add them to the txt file

--- functions.py
+++ functions.py
@@ -1,9 +1,9 @@
-def get_todos(filepath):
+def get_todos(filepath='todos.txt'):
     with open(filepath, 'r') as file:
         todos_local = file.readlines()
     return todos_local
 
 
-def save_todos(filepath, todos_to_save):
+def save_todos(todos_to_save, filepath='todos.txt'):
     with open(filepath, 'w') as file:
         file.writelines(todos_to_save)
--- gui.py
+++ gui.py
@@ -2,9 +2,23 @@ import functions
 import PySimpleGUI
 
 label = PySimpleGUI.Text("Type in a to-do")
-input_box = PySimpleGUI.InputText(tooltip="Enter todo")
+input_box = PySimpleGUI.InputText(tooltip="Enter todo", key='todo')
 add_button = PySimpleGUI.Button('Add')
 
-window = PySimpleGUI.Window('My To-Do App', layout=[[label], [input_box, add_button]])
-window.read()
+window = PySimpleGUI.Window('My To-Do App',
+                            layout=[[label], [input_box, add_button]],
+                            font=("Helvetica", 12))
+
+while True:
+    event, values = window.read()
+    match event:
+        case 'Add':
+            todos = functions.get_todos()
+            new_todo = values['todo'] + '\n'
+            todos.append(new_todo)
+            functions.save_todos(todos)
+        case PySimpleGUI.WIN_CLOSED:
+            break
+
+
 window.close()
