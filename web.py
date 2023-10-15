3a5dd1209d9f08abeeba246c47e638f29d1587da
Kyle Nichols <kyle.r.nichols@gmail.com>
2023-10-15 11:58:39 +0900

    modified the add todo bar so it clears after pressing enter

--- web.py
+++ web.py
@@ -3,19 +3,23 @@ import functions
 
 todos = functions.get_todos()
 def add_todo():
-    todo = st.session_state["new_todo"] + "\n"
-    todos.append(todo)
+    todo_to_add = st.session_state["new_todo"] + "\n"
+    todos.append(todo_to_add)
     functions.save_todos(todos)
+    st.session_state["new_todo"] = ""
 
-
-todos = functions.get_todos()
 
 st.title("My Todo App")
-st.subheader("This is my todo list")
-st.write("This app is cool")
+st.subheader("This app was created with streamlit")
+st.write("It's lit")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.save_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="", placeholder="Enter a todo",
               on_change=add_todo, key='new_todo')
